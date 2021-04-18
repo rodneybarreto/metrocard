8370a5b7be03636e2be54a79bc9829ef0b6b0500
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-04-18 16:03:44 -0300

    Refactor method create

--- src/test/java/br/uece/metrocard/service/TravelServiceTest.java
+++ src/test/java/br/uece/metrocard/service/TravelServiceTest.java
@@ -4,12 +4,15 @@ import br.uece.metrocard.domain.dto.TravelDto;
 import br.uece.metrocard.domain.entity.Card;
 import br.uece.metrocard.domain.entity.Travel;
 import br.uece.metrocard.domain.enums.Tariff;
+import br.uece.metrocard.repository.CardRepository;
 import br.uece.metrocard.repository.TravelRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -21,21 +24,28 @@ public class TravelServiceTest {
     @Mock
     private TravelRepository travelRepository;
 
+    @Mock
+    private CardRepository cardRepository;
+
     @InjectMocks
     private TravelService travelService;
 
     @DisplayName("Deve cadastrar uma viagem")
     @Test
-    public void create() {
+    public void create() throws Exception {
         TravelDto travelReq = new TravelDto();
         travelReq.setTariff("ZONE_A_UNIC");
         travelReq.setCardId(1);
 
+        Optional<Card> optional = Optional.of(new Card(1));
+        Card card = optional.get();
+
         Travel travel = new Travel();
         travel.setId(1);
         travel.setTariff(Tariff.ZONE_A_UNIC);
-        travel.setCard(new Card(1));
+        travel.setCard(card);
 
+        when(cardRepository.findById(any(Integer.class))).thenReturn(Optional.of(card));
         when(travelRepository.save(any(Travel.class))).thenReturn(travel);
 
         TravelDto travelRes = travelService.create(travelReq);
