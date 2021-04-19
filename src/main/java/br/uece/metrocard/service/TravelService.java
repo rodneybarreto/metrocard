009886afcad8b8e932097a5345ad4615c6b1eceb
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-04-19 09:58:55 -0300

    Validate debit when tariff ZONE_A_UNIC

--- src/main/java/br/uece/metrocard/service/TravelService.java
+++ src/main/java/br/uece/metrocard/service/TravelService.java
@@ -1,14 +1,17 @@
 package br.uece.metrocard.service;
 
 import br.uece.metrocard.domain.dto.TravelDto;
+import br.uece.metrocard.domain.entity.Account;
 import br.uece.metrocard.domain.entity.Card;
 import br.uece.metrocard.domain.entity.Travel;
 import br.uece.metrocard.domain.enums.Tariff;
+import br.uece.metrocard.repository.AccountRepository;
 import br.uece.metrocard.repository.CardRepository;
 import br.uece.metrocard.repository.TravelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.stream.Stream;
 
 @Service
@@ -16,24 +19,35 @@ public class TravelService {
 
     private static final String ZONE_A = "A";
     private static final String ZONE_B = "B";
+    private static final String ZONE_A_UNIC = "ZONE_A_UNIC";
 
     private final TravelRepository travelRepository;
     private final CardRepository cardRepository;
+    private final AccountRepository accountRepository;
 
     @Autowired
-    public TravelService(TravelRepository travelRepository, CardRepository cardRepository) {
+    public TravelService(TravelRepository travelRepository,
+                         CardRepository cardRepository,
+                         AccountRepository accountRepository) {
         this.travelRepository = travelRepository;
         this.cardRepository = cardRepository;
+        this.accountRepository = accountRepository;
     }
 
+    @Transactional
     public TravelDto create(TravelDto travelDto) throws Exception {
         Card card = getCard(travelDto);
+        Tariff tariff = getTariff(travelDto);
 
         if (card.getZoneType().equals(ZONE_A)) {
 
-            Tariff tariff = getTariff(travelDto);
             if (tariff.getZone().equals(ZONE_B)) {
                 throw new RuntimeException("Viagem não autorizada: a zona B não é permitida para este cartão.");
+            }
+
+            if (tariff.toString().equals(ZONE_A_UNIC)) {
+                Account account = card.getAccount();
+                account.debit(tariff.getValue());
             }
 
         }
