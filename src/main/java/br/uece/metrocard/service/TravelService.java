49fe84c90088b61391a883a7c047d6729cd7ff42
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-04-19 09:14:43 -0300

    Validate travels with different zones

--- src/main/java/br/uece/metrocard/service/TravelService.java
+++ src/main/java/br/uece/metrocard/service/TravelService.java
@@ -9,11 +9,16 @@ import br.uece.metrocard.repository.TravelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.stream.Stream;
+
 @Service
 public class TravelService {
 
-    private TravelRepository travelRepository;
-    private CardRepository cardRepository;
+    private static final String ZONE_A = "A";
+    private static final String ZONE_B = "B";
+
+    private final TravelRepository travelRepository;
+    private final CardRepository cardRepository;
 
     @Autowired
     public TravelService(TravelRepository travelRepository, CardRepository cardRepository) {
@@ -23,6 +28,15 @@ public class TravelService {
 
     public TravelDto create(TravelDto travelDto) throws Exception {
         Card card = getCard(travelDto);
+
+        if (card.getZoneType().equals(ZONE_A)) {
+
+            Tariff tariff = getTariff(travelDto);
+            if (tariff.getZone().equals(ZONE_B)) {
+                throw new RuntimeException("Viagem não autorizada: a zona B não é permitida para este cartão.");
+            }
+
+        }
 
         Travel travel = new Travel();
         travel.setCard(card);
@@ -36,6 +50,13 @@ public class TravelService {
         return cardRepository
                 .findById(travelDto.getCardId())
                 .orElseThrow(() -> new RuntimeException("Viagem não autorizada: cartão não encontrado."));
+    }
+
+    private Tariff getTariff(TravelDto travelDto) {
+        return Stream.of(Tariff.values())
+                .filter(t -> t.toString().equals(travelDto.getTariff()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Viagem não autorizada: a zona informada não foi encontrada."));
     }
 
 }
