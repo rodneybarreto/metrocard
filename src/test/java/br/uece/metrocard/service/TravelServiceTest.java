44fbff3522f00797e6edc10c3edc00e103e1fce1
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-04-20 15:40:01 -0300

    New test try using card zone A on zone B return exception

--- src/test/java/br/uece/metrocard/service/TravelServiceTest.java
+++ src/test/java/br/uece/metrocard/service/TravelServiceTest.java
@@ -13,9 +13,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -31,9 +32,9 @@ public class TravelServiceTest {
     @InjectMocks
     private TravelService travelService;
 
-    @DisplayName("Deve cadastrar uma viagem")
+    @DisplayName("Cadastra uma viagem na zona A ou B com tarifa única")
     @Test
-    public void create() throws Exception {
+    public void create1() throws Exception {
         TravelDto travelReq = new TravelDto();
         travelReq.setTariff("ZONE_A_UNIC");
         travelReq.setCardId(1);
@@ -51,6 +52,9 @@ public class TravelServiceTest {
         travel.setId(1);
         travel.setTariff(Tariff.ZONE_A_UNIC);
         travel.setCard(card);
+        travel.setTravelDate(LocalDate.now());
+        travel.setCheckin(true);
+        travel.setCheckout(false);
 
         when(cardRepository.findById(any(Integer.class))).thenReturn(Optional.of(card));
         when(travelRepository.save(any(Travel.class))).thenReturn(travel);
@@ -58,6 +62,30 @@ public class TravelServiceTest {
         TravelDto travelRes = travelService.create(travelReq);
         assertEquals(1, travelRes.getId());
         assertEquals(1, travelRes.getCardId());
+    }
+
+    @DisplayName("Retorna uma exceção na tentativa de usar cartão zona A na zona B")
+    @Test
+    public void create2() {
+        TravelDto travelReq = new TravelDto();
+        travelReq.setTariff("ZONE_B_UNIC");
+        travelReq.setCardId(1);
+
+        Account account = new Account();
+        account.setId(1);
+        account.setOwner("Bob");
+        account.setBalance(6.0);
+
+        Card card = new Card(travelReq.getCardId());
+        card.setZoneType("A");
+        card.setAccount(account);
+
+        when(cardRepository.findById(any(Integer.class))).thenReturn(Optional.of(card));
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            TravelDto travelRes = travelService.create(travelReq);
+        });
+        assertTrue(exception.getMessage().contains("zona B não é permitida para este cartão."));
     }
 
 }
