a65e66a011d4e47765794b2b3edda67ed40e0b38
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-04-30 15:56:28 -0300

    Remove unnecessary import

--- src/main/java/br/uece/metrocard/controller/LoginController.java
+++ src/main/java/br/uece/metrocard/controller/LoginController.java
@@ -6,7 +6,6 @@ import br.uece.metrocard.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
